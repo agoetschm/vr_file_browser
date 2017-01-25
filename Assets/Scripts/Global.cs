85e2a9cacfbda5f4de7c9dbaeadaacbde88301ee
agoetschm <anselme.goetschmann@gmail.com>
2017-01-24 19:01:22 -0500

    Before some cleaning

---
+++ Assets/Scripts/Global.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class Global : MonoBehaviour {
+
+
+	public Transform firstFolder;
+	public float smoothTime = .1f;
+
+	// object selected
+	private Transform actualFocus;
+	private float actualLevel = 1;
+
+	private float startTime = 0f;
+	private Vector3 targetScale;
+
+	private Vector3 targetPosition;
+	private Quaternion targetRotation;
+
+	public void setFocus (Transform newFocus) {
+		startTime = Time.time;
+
+		// get zoom factor
+		float levelDiff = newFocus.GetComponent<ObjectController> ().level / actualLevel;
+		float zoomFactor = levelDiff;//Mathf.Pow (5, levelDiff);
+
+		// set vars to next focus
+		actualFocus = newFocus;
+		actualLevel = newFocus.GetComponent<ObjectController> ().level;
+
+		// compute new scale, rotation and pos
+		targetScale = transform.localScale * zoomFactor;
+		Quaternion rotation = Quaternion.Inverse (actualFocus.rotation);
+		targetRotation = rotation * transform.rotation;
+		targetPosition = rotation * (transform.position - actualFocus.position) * zoomFactor;
+
+		// go through all object and delete/activate the right ones
+		foreach (Transform subObject in transform) {
+			ObjectController subScript = subObject.GetComponent<ObjectController> ();
+			float subLevel = subScript.level;
+			// delete the one of higher level not focused
+			if (subLevel > actualLevel && subScript.enclosing != actualFocus)
+				Destroy (subObject.gameObject);
+			// activate the one of the same level and not focused
+			else if( subLevel == actualLevel && subObject != actualFocus)
+				subObject.gameObject.SetActive(true);
+		}
+	}
+
+	void Awake () {
+		Transform folderClone = (Transform) Instantiate (firstFolder, transform);
+		//folderClone.localPosition += new Vector3 (0f, 0f, 4);
+		Camera.main.transform.position = new Vector3 (0f, 0f, -1f);
+
+		// adjust camera at begining
+		//CameraFollowsTarget followScript = Camera.main.GetComponent<CameraFollowsTarget>();
+		//followScript.setTarget (transform.position - transform.rotation * new Vector3(0f, 0f, 4), transform.position, transform.up);
+
+		setFocus(folderClone);
+		folderClone.gameObject.GetComponent<ObjectController> ().path = "/";
+		DirectoryInfo dir = new DirectoryInfo("/");
+		folderClone.GetChild (0).GetComponent<TextMesh> ().text = dir.FullName;
+
+	}
+
+	void Update () {
+		float t = Time.time - startTime;
+
+		Vector3 next = Vector3.Slerp (transform.localScale, targetScale, t * smoothTime);
+		/*if (next == transform.localScale) {
+			if (Input.GetKeyDown ("space"))
+				startRescale ();
+		} else */
+		transform.localScale = next;
+
+		transform.position = Vector3.Slerp (transform.position, targetPosition, t * smoothTime);
+		transform.rotation = Quaternion.Slerp (transform.rotation, targetRotation, t * smoothTime);
+	}
+}
---
+++ Assets/Scripts/ObjectController.cs
@@ -0,0 +1,177 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class ObjectController : MonoBehaviour {
+
+	public Transform subObjectPrefab;
+	public Transform textPrefab;
+
+
+	// level of the object to which this script is attached
+	public float level = 1f;
+	// enclosing folder of the object
+	public Transform enclosing;
+	// path of the file/folder represented
+	public string path = ".";
+	// foler or file
+	public bool isFolder = true;
+	// previous button
+	public bool isPrev = false;
+	// fraction of an enclosing file/folder
+	public float resize_factor;
+
+
+	private float FOV = 60 * Mathf.PI / 180;//100; // "field of view" -> angle covered by the objects from the point of view of the user
+
+	void OnMouseDown()
+	{
+		if (isFolder) {
+			// global script
+			Global globalScript = transform.parent.GetComponent<Global> ();
+
+			// if up
+			if (isPrev) {
+				// if it's not the top folder
+				if (enclosing.gameObject.GetComponent<ObjectController> ().enclosing != null) {
+					enclosing.gameObject.SetActive (true);
+					globalScript.setFocus (enclosing.gameObject.GetComponent<ObjectController> ().enclosing);
+				}
+			} else { // else explore folder			
+				DirectoryInfo dir = new DirectoryInfo (path);
+				FileSystemInfo[] info = dir.GetFileSystemInfos ();
+				IEnumerator fileEnumerator = info.GetEnumerator ();
+
+				// choose size of the file grid
+				float sqrt = Mathf.Sqrt (info.Length + 1);
+				int col_num;
+				if (sqrt > 4)
+					col_num = Mathf.CeilToInt (sqrt);
+				else
+					col_num = 4;
+				
+				int row_num = col_num; // => each sub-object has the same proportion as its parent => same number of rows as columns
+				resize_factor = col_num + 1;
+
+				// some intermediary values (cf schema)
+				float w = transform.lossyScale.x;
+				float h = transform.lossyScale.y;
+				float d = (w / 2) / Mathf.Tan (FOV / 2);
+				float r = d / Mathf.Cos (FOV / 2);
+				float fov_vertical = Mathf.Atan (h / 2 / d) * 2;
+
+				// TODO
+				bool first = true;
+
+				// "split" the object in (COL_NUM * row_num) smaller ones
+				for (int j = row_num - 1; j >= 0; --j) {
+					// x angle, same for the row
+					float xAngle = (-fov_vertical / 2) + (j + 0.5f) * (fov_vertical / row_num);
+					//Debug.Log (xAngle / Mathf.PI * 180);
+
+					for (int i = 0; i < col_num; ++i) {
+						// y angle (=> around y axis)
+						float yAngle = (-FOV / 2) + (i + 0.5f) * (FOV / col_num);
+
+						if ((first && dir.Parent != null) || fileEnumerator.MoveNext ()) { // no MoveNext if first and not root
+							FileSystemInfo actFile;
+							if (first && dir.Parent != null) {
+								actFile = dir.Parent;
+							} else {
+								if (first)
+									first = false; // no prev button in first level
+								actFile = (FileSystemInfo)fileEnumerator.Current;
+								//Debug.Log (actFile.Name);
+							}
+
+							// adjust the position to fit the curve
+							Vector3 relPos = new Vector3 (0f, 0f, -d); // go to "camera position"
+							relPos += new Vector3 (
+								r * Mathf.Sin (yAngle) * Mathf.Cos (xAngle), 
+								r * Mathf.Sin (xAngle), 
+								r * Mathf.Cos (yAngle) * Mathf.Cos (xAngle)); // go to position 
+							Vector3 subPos = transform.position + transform.rotation * relPos;
+
+							// instantiate the sub object
+							Transform subObject = (Transform)Instantiate (subObjectPrefab, subPos, 
+								                     transform.rotation * Quaternion.Euler (-xAngle / Mathf.PI * 180, yAngle / Mathf.PI * 180, 0f)//);
+						, transform.parent);
+
+
+							// TODO
+							// rescale it
+							subObject.localScale /= resize_factor;
+
+							// assign script vars
+							ObjectController subScript = subObject.GetComponent<ObjectController> ();
+							subScript.level = level * resize_factor;
+							subScript.enclosing = transform;
+							subScript.path = actFile.FullName;
+							subScript.isFolder = Directory.Exists (subScript.path);
+
+							// set text
+							string pathText;
+							if (first) {
+								first = false;
+								pathText = "..";
+								subScript.isPrev = true;
+							} else
+								pathText = actFile.Name;
+							Transform textTransform = subObject.GetChild (0); // should be only the text as child
+							TextMesh textMesh = textTransform.GetComponent<TextMesh> ();
+							textMesh.text = pathText;
+
+							// TODO sub routine
+							// manage overflow
+							// try to replace spaces first
+							float limit = subObject.GetComponent<Renderer> ().bounds.size.x * 0.8f;
+							if (textMesh.GetComponent<Renderer> ().bounds.size.x > limit) {
+								pathText = pathText.Replace (' ', '\n');
+								textMesh.text = pathText;
+							} // then the dots 
+							if (textMesh.GetComponent<Renderer> ().bounds.size.x > limit) {
+								pathText = pathText.Replace (".", "\n.");
+								textMesh.text = pathText;
+							} // finally limit to 7 char per line (I know, it's not precise...)
+							if (textMesh.GetComponent<Renderer> ().bounds.size.x > limit) {
+								string[] lines = pathText.Split ('\n');
+								pathText = "";
+								foreach (string line in lines) {
+									int len = line.Length;
+									for (int k = 0; k < len; k += 7) {
+										pathText += line.Substring (k, Mathf.Min (7, len - k));
+										pathText += "\n";
+									}
+								}
+								pathText = pathText.Trim (); // remove \n at the end
+								textMesh.text = pathText;
+							}
+
+
+
+							// set color
+							MeshRenderer meshRenderer = subObject.GetComponent<MeshRenderer> ();
+							meshRenderer.material.shader = Shader.Find ("Self-Illumin/VertexLit");//("Transparent/Bumped Diffuse");
+							if (subScript.isFolder)
+								meshRenderer.material.color = new Color (13f / 255, 71f / 255, 161f / 255, .5f);
+							else
+								meshRenderer.material.color = new Color (191f / 255, 54f / 255, 12f / 255, .5f);
+						}
+					}
+				}
+
+
+				// move camera
+				//CameraFollowsTarget followScript = Camera.main.GetComponent<CameraFollowsTarget>();
+				//followScript.setTarget (transform.position - transform.rotation * new Vector3(0f, 0f, d*2), transform.position, transform.up);
+
+				// disable enclosing object
+				gameObject.SetActive (false);
+
+
+				// start the rescale simulating the zoom (because the camera doesn't "see" objects too close)
+				globalScript.setFocus (transform);
+			}
+		}
+	}
+}
